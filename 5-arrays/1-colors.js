20461c65374b9427eed8ecb7477948a1e0e2fed4
Jose Cuevas <josecueram@gmail.com>
2020-07-29 14:46:34 -0500

    [ADD] Example in Array Exercise

--- 5-arrays/1-colors.js
+++ 5-arrays/1-colors.js
@@ -2,3 +2,9 @@
 // * first fordwards (1...10), then backwards (10...1)
 
 const colors = ['Blue', 'Greeb',  'Yellow', 'Red', 'Purple', 'Brown', 'Orange']
+
+// -> 0...10
+for (let i = 0; i < colors.length; i++) {}
+
+// <- 10...0
+for (let i = colors.length - 1; i >= 0; i--) {}
